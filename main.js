72a2a90b27db521307406aa64dfcd120475008a7
Tim-KoT <136622477+Tim-KoT@users.noreply.github.com>
2026-01-28 20:58:06 +0300

    1

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-console.log("Привет мир");
-let x = "Привет мир!!!";
-console.log(x)
-let valuePromt = promt("введите число", 50);
-alert(valuePromt)
-console.log(+valuePromt+50)
+// console.log("Привет мир");
+// let x = "Привет мир!!!";
+// console.log(x)
+// let valuePromt = promt("введите число", 50);
+// alert(valuePromt)
+// console.log(+valuePromt+50)
+// valuePromt = +promt("Введите ваше число")
+// alert(valuePromt**2)
+
+// x = +prompt("введите ваше 1 число");
+// y = +prompt("введите ваше 2 число");
+// let average = (x+y)/2;
+// alert(average);
+x = +prompt("введите длину стороны квадрата");
+let s = x**2
+alert("Площадь квадрата равна" + s)
+let name = prompt("Введите имя")
+alert("Привет" + name)
