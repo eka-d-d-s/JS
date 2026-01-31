079cda6d2a058496050ae4418bf2ceb16ee51c41
eka-d-d-s <www.eka-d-d-s@mail.ru>
2026-01-31 13:10:51 +0300

    dom na 5.02

--- main.js
+++ main.js
@@ -11,8 +11,23 @@
 // y = +prompt("введите ваше 2 число");
 // let average = (x+y)/2;
 // alert(average);
-x = +prompt("введите длину стороны квадрата");
-let s = x**2
-alert("Площадь квадрата равна" + s)
-let name = prompt("Введите имя")
-alert("Привет" + name)
+// x = +prompt("введите длину стороны квадрата");
+// let s = x**2
+// alert("Площадь квадрата равна" + s)
+// let name = prompt("Введите имя")
+// alert("Привет" + name)
+// Домашнее задание на 5.02
+//1
+let imia = prompt("Введите имя")
+alert("Привет " + imia + " !")
+//2
+let god = +prompt("Введите ваш год рождения")
+alert (2025 - god)
+//3
+x = +prompt("Введите длину квадрата")
+let p = x * 4
+alert(p)
+//4
+x = +prompt("Введите радиус окружности")
+let s = x**2 * 3.14
+alert (s)
