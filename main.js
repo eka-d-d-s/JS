ee29c02c2e2b3fa829db9debf1eb9c8532e2bdd5
eka-d-d-s <www.eka-d-d-s@mail.ru>
2026-03-03 13:11:51 +0300

    1

--- main.js
+++ main.js
@@ -625,7 +625,7 @@ setInterval(timer,1000);
 function timer() {
     let now = new Date();
     let end = new Date(
-        now.getUTCFullYear(),
+        now.getFullYear(),
         5,
         1,
         0,
